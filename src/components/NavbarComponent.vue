0e987a738a59865f71753f96375bf141765dad29
Anu Thampi A S <anu.thampi@hatio.in>
2024-12-04 10:33:29 +0530

    initial configurations done

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <ul class="">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/income">Income</router-link></li>
+      <li><router-link to="/expense">Expenses</router-link></li>
+    </ul>
+  </nav>
+</template>
